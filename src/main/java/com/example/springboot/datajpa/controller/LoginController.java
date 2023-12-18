9a77da20ca3cca4a89a19f72ed91a1b6b5bd0606
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-18 20:49:24 +0700

    54. Validating LoginController with error message

--- src/main/java/com/example/springboot/datajpa/controller/LoginController.java
+++ src/main/java/com/example/springboot/datajpa/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.springboot.datajpa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,10 +12,14 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash) {
+    public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes flash) {
         if(principal != null) {
             flash.addFlashAttribute("info","you have already logged in before");
             return "redirect:/";
+        }
+
+        if(error != null) {
+            model.addAttribute("error", "Login error: Incorrect username or password, please try again");
         }
         return "login";
     }
