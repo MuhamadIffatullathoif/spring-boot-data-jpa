dc74bfcb80cdc4ccba7678d1f4fd59b07b7d0c09
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-20 08:39:43 +0700

    66. Export CSV : implement export csv

---
+++ src/main/java/com/example/springboot/datajpa/view/csv/CustomerCsvView.java
@@ -0,0 +1,44 @@
+package com.example.springboot.datajpa.view.csv;
+
+import com.example.springboot.datajpa.domain.Customer;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import java.util.Map;
+
+@Component("list.csv")
+public class CustomerCsvView extends AbstractView {
+
+    public CustomerCsvView() {
+        setContentType("text/csv");
+    }
+
+    @Override
+    protected boolean generatesDownloadContent() {
+        return true;
+    }
+
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setHeader("Content-Disposition", "attachment; filename=\"customer.csv\"");
+        response.setContentType(getContentType());
+
+        Page<Customer> customers = (Page<Customer>) model.get("customers");
+
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] header = {"id", "name", "lastName","email","createdAt"};
+        beanWriter.writeHeader(header);
+
+        for (Customer customer: customers) {
+            beanWriter.write(customer, header);
+        }
+
+        beanWriter.close();
+    }
+}
